fad0b11fb7285b7c2a3572b14c70d8972e6a73d9
Kamil Sarna <kamil.sarna@gmail.com>
2016-04-19 15:27:05 +0200

    Add module for update fb pixel js

---
+++ lib/rack/tracker/facebook_pixel_2/facebook_pixel_2.rb
@@ -0,0 +1,29 @@
+class Rack::Tracker::FacebookPixel2 < Rack::Tracker::Handler
+  class Event < OpenStruct
+    def write
+      [type_to_json, options_to_json].compact.join(', ')
+    end
+
+    private
+
+    def type_to_json
+      self.type.to_json
+    end
+
+    def options_to_json
+      return nil unless self.options.present?
+
+      self.options.to_json
+    end
+  end
+
+  self.position = :body
+
+  def render
+    Tilt.new( File.join( File.dirname(__FILE__), 'template/facebook.erb') ).render(self)
+  end
+
+  def self.track(name, *event)
+    { name.to_s => [event.last.merge('class_name' => 'Event')] }
+  end
+end
